26bd0917051ea0b61b7dd0a8a577d5e53953cb92
Kaycksanttos <kaycksanttos0@gmail.com>
2023-07-15 20:22:10 -0300

    Botão de cadastro agora salva os produtops

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,100 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-
+import java.util.List;
 
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
-    }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
-}
 
+    public int cadastrarProduto(ProdutosDTO produto) {
+        int status;
+        conn = new conectaDAO().connectDB();
+
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            status = prep.executeUpdate();
+            return status;
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+            return e.getErrorCode();
+        }
+
+    }
+
+    public List<ProdutosDTO> listarProdutos() {
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos";
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            List<ProdutosDTO> listProdutos = new ArrayList<>();
+
+            while (resultset.next()) {
+                ProdutosDTO produtosDTO = new ProdutosDTO();
+
+                produtosDTO.setId(resultset.getInt("id"));
+                produtosDTO.setNome(resultset.getString("nome"));
+                produtosDTO.setValor(resultset.getInt("valor"));
+                produtosDTO.setStatus(resultset.getString("status"));
+
+                listProdutos.add(produtosDTO);
+            }
+            return listProdutos;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public int venderProduto(ProdutosDTO produtosDTO) {
+        int status;
+        conn = new conectaDAO().connectDB();
+        try {
+            prep = conn.prepareStatement("UPDATE produtos SET status = ? WHERE id = ?");
+            prep.setString(1, produtosDTO.getStatus());
+            prep.setInt(2, produtosDTO.getId());
+            status = prep.executeUpdate();
+            return status;
+        } catch (SQLException e) {
+            System.out.println(e.getErrorCode());
+            return e.getErrorCode();
+        }
+    }
+
+    public List<ProdutosDTO> listarProdutoVendido() {
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos WHERE status LIKE 'Vendido'";
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            List<ProdutosDTO> listProdutos = new ArrayList<>();
+
+            while (resultset.next()) {
+                ProdutosDTO produtosDTO = new ProdutosDTO();
+
+                produtosDTO.setId(resultset.getInt("id"));
+                produtosDTO.setNome(resultset.getString("nome"));
+                produtosDTO.setValor(resultset.getInt("valor"));
+                produtosDTO.setStatus(resultset.getString("status"));
+
+                listProdutos.add(produtosDTO);
+            }
+            return listProdutos;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -1,18 +1,17 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
 public class listagemVIEW extends javax.swing.JFrame {
-
     /**
      * Creates new form listagemVIEW
      */
@@ -135,23 +134,28 @@ public class listagemVIEW extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
-        String id = id_produto_venda.getText();
-        
-        ProdutosDAO produtosdao = new ProdutosDAO();
-        
-        //produtosdao.venderProduto(Integer.parseInt(id));
-        listarProdutos();
-    }//GEN-LAST:event_btnVenderActionPerformed
-
-    private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-        //vendasVIEW vendas = new vendasVIEW(); 
-        //vendas.setVisible(true);
-    }//GEN-LAST:event_btnVendasActionPerformed
-
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
         this.dispose();
     }//GEN-LAST:event_btnVoltarActionPerformed
+
+    private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
+        vendasVIEW vendas = new vendasVIEW();
+        vendas.setVisible(true);
+    }//GEN-LAST:event_btnVendasActionPerformed
+
+    private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
+        String id = id_produto_venda.getText();
+
+        ProdutosDAO produtosdao = new ProdutosDAO();
+        ProdutosDTO produtosDTO = new ProdutosDTO();
+
+        produtosDTO.setId(Integer.parseInt(id));
+        produtosDTO.setStatus("Vendido");
+
+        produtosdao.venderProduto(produtosDTO);
+        listarProdutos();
+        id_produto_venda.setText("");
+    }//GEN-LAST:event_btnVenderActionPerformed
 
     /**
      * @param args the command line arguments
@@ -201,16 +205,16 @@ public class listagemVIEW extends javax.swing.JFrame {
     private javax.swing.JTable listaProdutos;
     // End of variables declaration//GEN-END:variables
 
-    private void listarProdutos(){
+    private void listarProdutos() {
         try {
             ProdutosDAO produtosdao = new ProdutosDAO();
-            
+
             DefaultTableModel model = (DefaultTableModel) listaProdutos.getModel();
             model.setNumRows(0);
-            
-            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutos();
-            
-            for(int i = 0; i < listagem.size(); i++){
+
+            List<ProdutosDTO> listagem = produtosdao.listarProdutos();
+
+            for (int i = 0; i < listagem.size(); i++) {
                 model.addRow(new Object[]{
                     listagem.get(i).getId(),
                     listagem.get(i).getNome(),
@@ -220,6 +224,6 @@ public class listagemVIEW extends javax.swing.JFrame {
             }
         } catch (Exception e) {
         }
-    
+
     }
 }
