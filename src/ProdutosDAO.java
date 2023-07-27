e714c19d435dc51329ce836d1c81b7aa1f6608cf
Kaycksanttos <kaycksanttos0@gmail.com>
2023-07-27 12:26:09 -0300

    Correção em <ProdutosDAO>

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,7 +12,9 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProdutosDAO {
@@ -23,20 +25,48 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
+     
         //conn = new conectaDAO().connectDB();
-        
-        
+      
+        int status;
+        conn = new conectaDAO().connectDB();
+
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            status = prep.executeUpdate();
+            return status;
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+            return e.getErrorCode();
+        }
+
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
-}
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos WHERE status LIKE 'Vendido'";
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            List<ProdutosDTO> listProdutos = new ArrayList<>();
 
+            while (resultset.next()) {
+                ProdutosDTO produtosDTO = new ProdutosDTO();
+
+                produtosDTO.setId(resultset.getInt("id"));
+                produtosDTO.setNome(resultset.getString("nome"));
+                produtosDTO.setValor(resultset.getInt("valor"));
+                produtosDTO.setStatus(resultset.getString("status"));
+
+                listProdutos.add(produtosDTO);
+            }
+            return listProdutos;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
